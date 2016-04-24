babb93f4a1d1252f1830032525fdc5499ea1bd33
uazori <uazori@gmail.com>
2016-04-24 10:13:58 +0300

    Ant with Ivy   and Maven aded

--- CollectionsTest/src/main/java/Generics/PersonValidator.java
+++ CollectionsTest/src/main/java/Generics/PersonValidator.java
@@ -2,7 +2,7 @@ package Generics;
 
 
 
-import static org.apache.commons.lang3.StringUtils.*;
+//import static org.apache.commons.lang3.StringUtils.*;
 
 /**
  * Created by Vadim on 14.03.2016.
@@ -10,6 +10,6 @@ import static org.apache.commons.lang3.StringUtils.*;
 public class PersonValidator implements Validator<Person>{
     @Override
     public boolean isValid(Person value) {
-        return isBlank(value.name)&& isBlank(value.surname);
+        return true/*isBlank(value.name)&& isBlank(value.surname)*/;
     }
 }
